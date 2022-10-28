7f809c3f49c83532a927d05d0d6da78293d37f68
Gisell03 <102748916+Gisell03@users.noreply.github.com>
2022-10-27 22:06:47 -0500

    Update conexion.php

--- usuarios/conexion.php
+++ usuarios/conexion.php
@@ -1,4 +1,4 @@
 <?php
-  $conexion = mysqli_connect("localhost", "root", "", "usuarios") or
+  $conexion = mysqli_connect("b7ebj5x4vmkrzjzxbsyb-mysql.services.clever-cloud.com", "uq7biqqrwvtadijq", "TOlllRkGcFgCy9TtKkdz", "b7ebj5x4vmkrzjzxbsyb") or
   die("Problemas con la conexión");
 ?>
