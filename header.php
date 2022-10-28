cefeaccb5c07dafc86301bb44daa4657bf4629d3
Gisell03 <102748916+Gisell03@users.noreply.github.com>
2022-10-27 21:52:24 -0500

    Add files via upload

---
+++ header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link rel="stylesheet" href="css/header.css">
+</head>
+<body>
+  <nav>
+<a href="#">
+    <img src="img/logo.jpg" alt="logo" style="width:20px;">
+  </a>
+	<a href="index.php">inicio</a>
+	<a href="registro.php">Registrarte</a>
+	<div class="animation start-home"></div>
+</nav>
+</body>
+</html>
+
